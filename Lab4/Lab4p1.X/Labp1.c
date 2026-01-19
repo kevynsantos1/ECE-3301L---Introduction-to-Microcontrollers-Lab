a37ea97db23da1e68448b15e276304ff0ddf3af8
Kevyn Santos <kevynsantos1804@gmail.com>
2026-01-18 23:19:50 -0800

    Update Labp1.c

--- Lab4/Lab4p1.X/Labp1.c
+++ Lab4/Lab4p1.X/Labp1.c
@@ -5,39 +5,7 @@
 #define _XTAL_FREQ 64000000
 
 void main(void)
-{
-    /*OSCCON = 0b01110000;
-    OSCTUNE = 0b01000000;
-    
-    LCD_init();
-    int round = 0;
-    int counter = 0;
-    
-    
-    while(1)
-    {
-        
-        LCD_cursor_set(1,1);
-        LCD_write_string("Counter = ");
-        LCD_write_variable(counter,0);
-        
-        LCD_cursor_set(2,1);
-        LCD_write_string("Round = ");
-        LCD_write_variable(round,0);
-        
-        __delay_ms(500);
-        
-        counter++;
-        
-        if (counter >= 15)
-        {
-            counter = 0;
-            //when counter is set to 0 it doesnt clear the second digit in the lcd
-            round++;
-        }
-
-    }  */
-    
+{   
     OSCCON = 0b01110000;
     OSCTUNE= 0b01000000;
     ANSELB = 0;
@@ -71,4 +39,5 @@ void main(void)
     }
     
     
+
 }
